28efa82c6042cc1949e72784e8e1018321ad383d
gaurav wable <gaurav.wable@mobiliya.com>
2017-05-24 17:33:05 +0530

    initial commit for Xamarin Forms App. Functional Splash screen, Main screen for Dispatch and Receive options and Zxing library integration added. UI and Assets needs to be modified.

---
+++ AssetTracking/AssetTracking/pages/DispatchShipment.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace AssetTracking
+{
+	public partial class DispatchShipment : ContentPage
+	{
+		ZXingScannerView zxingView;
+
+		public DispatchShipment()
+		{
+			InitializeComponent();
+			ScanCode();
+		}
+
+		async void OnTappedLink(object sender, EventArgs args)
+		{
+			await DisplayAlert("Clicked!", "Do you want to link?", "OK");
+		}
+
+		async void OnTappedScanShipment(object sender, EventArgs args)
+		{
+			await DisplayAlert("Clicked!", "Do you want to scan shipment?", "OK");
+		}
+
+		async void OnTappedScanSensor(object sender, EventArgs args)
+		{
+			await DisplayAlert("Clicked!", "Do you want to Scan Sensor?", "OK");
+			/*		var scanPage = new ZXingScannerPage();
+
+					scanPage.OnScanResult += (result) =>
+					{
+						// Stop scanning
+						scanPage.IsScanning = false;
+
+						// Pop the page and show the result
+						Device.BeginInvokeOnMainThread(() =>
+						{
+							Navigation.PopModalAsync();
+							DisplayAlert("Scanned Barcode", result.Text, "OK");
+						});
+					};
+
+					// Navigate to our scanner page
+					await Navigation.PushModalAsync(scanPage); */
+		}
+
+		void ScanCode() {
+			ContainerLayoutDetails.IsVisible = false;
+			ContainerLayoutScanner.IsVisible = true;
+			zxingView = new ZXingScannerView
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				AutomationId = "zxingScannerView",
+			};
+			zxingView.OnScanResult += (result) =>
+			{
+				// Stop scanning
+				//zxingView.IsScanning = false;
+
+				// Pop the page and show the result
+				Device.BeginInvokeOnMainThread(() =>
+				{
+					//Navigation.PopModalAsync();
+					//DisplayAlert("Scanned Barcode", result.Text, "OK");
+
+					zxingView.IsEnabled = false;
+					ContainerLayoutScanner.IsVisible = false;
+					ContainerLayoutDetails.IsVisible = true;
+				});
+			};
+
+			ScannerLayout.Children.Add(zxingView);
+			zxingView.IsEnabled = true;
+
+		}
+
+
+        protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			zxingView.IsScanning = true;
+		}
+
+		protected override void OnDisappearing()
+		{
+			zxingView.IsScanning = false;
+
+			base.OnDisappearing();
+		}
+
+		void Next(object sender, EventArgs args)
+		{
+			//await DisplayAlert("Clicked!", "Do you want to go Next?", "OK");
+			ScanCode();
+		}
+
+		async void Back(object sender, EventArgs args)
+		{
+			//await DisplayAlert("Clicked!", "Do you want to go Next?", "OK");
+			await Navigation.PopModalAsync();
+		}
+	}
+}
