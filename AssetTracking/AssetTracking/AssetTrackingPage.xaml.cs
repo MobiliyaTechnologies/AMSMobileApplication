0c7525bcbbf7dacb2c839b560eab0ce6ed7a32b0
gaurav wable <gaurav.wable@mobiliya.com>
2017-06-27 12:14:44 +0530

    Android App optimization

--- AssetTracking/AssetTracking/AssetTrackingPage.xaml.cs
+++ AssetTracking/AssetTracking/AssetTrackingPage.xaml.cs
@@ -1,24 +1,34 @@
 ﻿using AssetTracking.pages;
 using System;
 using Xamarin.Forms;
+using System.Threading.Tasks;
 
 namespace AssetTracking
 {
     public partial class AssetTrackingPage : ContentPage
     {
-        public AssetTrackingPage()
+		public AssetTrackingPage()
         {
             InitializeComponent();
+			TapGesture.Tapped += OnTappedDispatch;
+			TabReceive.Tapped += OnTappedReceive;
         }
 
-        async void OnTappedDispatch(object sender, EventArgs args)
+         async void OnTappedDispatch(object sender, EventArgs args)
         {
-            await Navigation.PushModalAsync(new DispatchShipment());
+			TapGesture.Tapped -= OnTappedDispatch;
+				await Navigation.PushModalAsync(new DispatchShipment());
+
+				TapGesture.Tapped += OnTappedDispatch;
+             
+
         }
 
         async void OnTappedReceive(object sender, EventArgs args)
         {
+			TabReceive.Tapped -= OnTappedReceive;
             await Navigation.PushModalAsync(new ReceiveShipment());
+			TabReceive.Tapped += OnTappedReceive;
         }
     }
 }
